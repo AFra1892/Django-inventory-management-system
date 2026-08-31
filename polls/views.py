a86158f1cefe102f23d84f050df1d9c65c6bfbdd
Amir Rafi <mashhadifarahani1@gmail.com>
2026-08-31 12:10:40 -0900

    end of part 3 (templates)

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.shortcuts import get_object_or_404 , render
+
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -9,8 +12,12 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return HttpResponse(template.render(context, request))
 
+
+
 def detail(request , question_id):
-    return HttpResponse("You're looking at question %s ." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
