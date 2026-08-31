2dcfd3dd3be98ac5422f7d28fc9444e6536ff9a7
Amir Rafi <mashhadifarahani1@gmail.com>
2026-08-30 18:08:59 -0900

    views render

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("" , views.index , name='index'),
+    path("" , views.index , name='index'), #example : /polls/
+    path("<int:question_id>/" , views.detail , name="detail"), #example: /pills/5/
+    path("<int:question_id>/results/" , views.results , name="results"), #example: polls/5/results/
+    path("<int:question_id>/vote/" , views.vote , name="vote"),
+
 ]
