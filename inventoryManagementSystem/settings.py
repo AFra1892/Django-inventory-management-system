0bf1e1b39be0e0f096fe2ef1bac776bbeeb859b6
Amir Rafi <mashhadifarahani1@gmail.com>
2026-09-01 12:15:14 -0900

    adding static files part 6

--- inventoryManagementSystem/settings.py
+++ inventoryManagementSystem/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'articles',
-    'polls.apps.PollsConfig',
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
